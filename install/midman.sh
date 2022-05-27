7347b282327c64dc428a0f18e1c60a2c55d887e0
Justine Dela Torre <97047918+Jasut1n@users.noreply.github.com>
2022-05-27 22:04:38 +0800

    ...

---
+++ install/midman.sh
@@ -0,0 +1,24 @@
+#/bin/env bash
+
+printf "\n\t\e[1;92mUPDATING THE INSTALLATION...\e[0m\n\n\n\n\n\n\n\n\n\n\n\n"
+
+sleep 3;
+
+rm --force install.sh
+
+sleep 2;
+
+printf "\n\n\t\e[1;92mRUN AS SUPERUSER TO RESUME THE INSTALLATION\n\n\n\e[0m"
+
+sleep 2;
+
+mv install/module.sh install/..
+
+mv module.sh install.sh
+chmod +x install.sh
+chmod +r install.sh
+
+sleep 2;
+
+sudo ./install.sh
+
